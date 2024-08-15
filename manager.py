9d634a43865528bf720b886d022e69a8be0286fd
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-08-14 21:49:38 -0600

    Import necessary modules

---
+++ manager.py
@@ -0,0 +1,12 @@
+"""
+Python Script for a password manager
+"""
+#Import necessary modules
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+import base64
+import os
+import secrets
+
